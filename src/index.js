7a97be2fdd511281653834b8808d5bed377beb3e
rodrigo-os <rodrigo.os743@gmail.com>
2023-02-02 15:17:22 -0300

    Projeto finalizado.

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
