25080fb0971b1c1e52f74e7b31e9846f0497740f
rodrigo-os <rodrigo.os743@gmail.com>
2023-02-02 02:39:13 -0300

    Implementando Google Autocomplete no cadastro de uma viagem.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import Modal from 'react-bootstrap/Modal';
 import Accordion from 'react-bootstrap/Accordion';
@@ -33,12 +33,73 @@ function App() {
   const handleShow = () => setShow(true);
   const [travels, setTravels] = useState([]);
 
+  const [fromAddress, setFromAddress] = useState({});
+  const [toAddress, setToAddress] = useState({});
+
+  const fromSearchInput = useRef(null);
+  const toSearchInput = useRef(null);
+
   const initMapScript = () => {
     if(window.google) {
       return Promise.resolve();
     }
     const src = `${mapApiJS}?key=${apiKey}&libraries=places&v=weekly`;
     return loadAsyncScript(src);
+  }
+
+  const onChangeAddress=(autocomplete, inputType)=>{
+    let sample = {
+      city: "",
+      state: "",
+      long: 0,
+      lat: 0,
+    }
+    const place = autocomplete.getPlace();
+    const placeType = inputType;
+    sample.long = place.geometry.viewport.Ia.hi;
+    sample.lat = place.geometry.viewport.Ya.hi;
+
+    place.address_components.forEach(addressComponent =>{
+      const types = addressComponent.types;
+      const long_name = addressComponent.long_name;
+      const short_name = addressComponent.short_name;
+      if(types.includes("locality")){
+        sample.city = long_name;
+      }else if(types.includes("administrative_area_level_2")){
+        sample.city = long_name;
+      }
+      if(types.includes("administrative_area_level_1")){
+        sample.state = short_name;
+      }
+    });
+    if(placeType = "FROM"){
+      setFromAddress(sample);
+    }else{
+      setToAddress(sample);
+    }
+  }
+
+  const initAutocomplete=()=>{
+    if(!fromSearchInput.current){
+      return
+    }else{
+      const fromAutocomplete = new window.google.maps.places.Autocomplete(
+        fromSearchInput.current);
+      fromAutocomplete.setFields(["address_component", "geometry"]);
+      fromAutocomplete.addListener("places_changed", ()=>{
+        onChangeAddress(fromAutocomplete, "FROM");
+      });
+    }
+    if(!toSearchInput.current){
+      return
+    }else{
+      const toAutocomplete = new window.google.maps.places.Autocomplete(
+        toSearchInput.current);
+      toAutocomplete.setFields(["address_component", "geometry"]);
+      toAutocomplete.addListener("place_changed", ()=>{
+        onChangeAddress(toAutocomplete, "TO");
+      });
+    }
   }
   
   async function getTravels() {
@@ -47,7 +108,7 @@ function App() {
   }
 
   useEffect(()=>{
-    initMapScript().then(()=>console.log("Map API Loaded"));
+    initMapScript().then(()=>initAutocomplete());
     getTravels();
   },[]);
 
@@ -118,11 +179,12 @@ function App() {
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Viagem</Modal.Title>
+          <Modal.Title>
+            Viagem</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-         <input className="inputModal" placeholder='Origem'></input>
-         <input className="inputModal" placeholder="Destino"></input>
+          <input className="inputModal" ref={fromSearchInput} placeholder='Origem'></input>
+          <input className="inputModal" ref={toSearchInput} placeholder="Destino"></input>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
@@ -131,17 +193,20 @@ function App() {
           <Button variant="primary">Salvar</Button>
         </Modal.Footer>
       </Modal>
-
       <header className="container_travels">
         <div className="header_travels">
           <h1>PLANO DE VIAGEM</h1>
         </div>
         <Travels travels={travels}></Travels>
         <Modal></Modal>
-        <button onClick={handleShow} class ="createTravelBtn">Nova Viagem</button>
+        <button onClick={()=>{
+          handleShow();
+          initMapScript().then(()=>initAutocomplete());
+        }} class ="createTravelBtn">
+          Nova Viagem
+        </button>
       </header>
     </div>
   );
 }
-
 export default App;
