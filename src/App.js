dd57409108dcb0260ba6959a5c1a4b017e46b394
rodrigo-os <rodrigo.os743@gmail.com>
2023-02-01 23:31:20 -0300

    Função responsável pela listagem de viagens criada.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Modal from 'react-bootstrap/Modal';
+import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import { 
   AiFillEnvironment,AiOutlineEnvironment,
   AiOutlineArrowRight,AiOutlineHourglass, 
   AiOutlineCar,AiFillDelete,AiFillEdit,
 } from "react-icons/ai";
 
-import React, { useState } from 'react';
-import Modal from 'react-bootstrap/Modal';
-import Accordion from 'react-bootstrap/Accordion';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-
-
 function App() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [travels, setTravels] = useState([]);
+  
+  async function getTravels() {
+    const response = await axios.get("http://localhost:3333/travels")
+    setTravels(response.data);
+  }
+
+  useEffect(()=>{
+    getTravels();
+  },[]);
+
+  const Travels = ({travels}) =>{
+    return (
+      <div className='travels'>
+        {travels.map(travel =>{
+          return (
+            <div className='travel'>
+              <Accordion>
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header className='Accordion-Header'>
+                    <div className='from'>
+                      <span>
+                        <AiFillEnvironment size={20}></AiFillEnvironment>
+                      </span>
+                      <p>{`${travel.travel.from.city}, ${travel.travel.from.state}`}</p>
+                    </div>
+                    <span className='arrowIcon'>
+                      <AiOutlineArrowRight size={20}></AiOutlineArrowRight>
+                    </span>
+                    <div className='to'>
+                      <span>
+                        <AiOutlineEnvironment size={20}></AiOutlineEnvironment>
+                      </span>
+                      <p>{`${travel.travel.to.city}, ${travel.travel.to.state}`}</p>
+                    </div>
+                  </Accordion.Header>
+                  <Accordion.Body className="Accordion-Body">
+                    <Card>
+                      <Card.Body class="AccordionCardBody">
+                        <Card.Title class="AccordionCardTitle">
+                          <div className='distance'>
+                            <AiOutlineCar size={19}></AiOutlineCar>
+                            <p>{`Distância: ${travel.travel.travel_distance}`}</p>
+                          </div>
+                          <div className="duration">
+                            <AiOutlineHourglass size={19}></AiOutlineHourglass>
+                            <p>{`Tempo de viagem: ${travel.travel.travel_duration}`}</p>
+                          </div>
+                        </Card.Title>
+                        <Card.Text class="AccordionCardText">
+                          <button className='deleteButton'>
+                            <AiFillDelete size={19}></AiFillDelete>
+                          </button>
+                          <button className='updateButton'>
+                            <AiFillEdit size={19}></AiFillEdit>
+                          </button>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-
       <Modal
         show={show}
         onHide={handleClose}
@@ -44,105 +112,7 @@ function App() {
         <div className="header_travels">
           <h1>PLANO DE VIAGEM</h1>
         </div>
-        <div className="travels">
-          <div className='travel'>
-            <Accordion>
-              <Accordion.Item eventKey="0">
-                <Accordion.Header className='Accordion-Header'>
-                  <div className='from'>
-                    <span>
-                      <AiFillEnvironment size={20}></AiFillEnvironment>
-                    </span>
-                    <p>Canoinhas, SC</p>
-                  </div>
-                  <span className='arrowIcon'>
-                    <AiOutlineArrowRight size={20}></AiOutlineArrowRight>
-                  </span>
-                  <div className='to'>
-                    <span>
-                      <AiOutlineEnvironment size={20}></AiOutlineEnvironment>
-                    </span>
-                    <p>Ponta Grossa, PR</p>
-                  </div>
-                </Accordion.Header>
-                <Accordion.Body className="Accordion-Body">
-                  <Card>
-                    <Card.Body class="AccordionCardBody">
-                      <Card.Title class="AccordionCardTitle">
-                        <div className='distance'>
-                          <AiOutlineCar size={19}></AiOutlineCar>
-                          <p>Distância: 167,2 KM</p>
-                        </div>
-                        
-                        <div className="duration">
-                          <AiOutlineHourglass size={19}></AiOutlineHourglass>
-                          <p>Tempo de viagem: 3H</p>
-                        </div>
-                      </Card.Title>
-                      <Card.Text class="AccordionCardText">
-                        <button className='deleteButton'>
-                          <AiFillDelete size={19}></AiFillDelete>
-                        </button>
-                        <button variant="outline-dark" className='updateButton'>
-                          <AiFillEdit size={19}></AiFillEdit>
-                        </button>
-                      </Card.Text>
-                    </Card.Body>
-                  </Card>
-                </Accordion.Body>
-              </Accordion.Item>
-            </Accordion>
-          </div>
-          <div className='travel'>
-            <Accordion>
-              <Accordion.Item eventKey="0">
-                <Accordion.Header className='Accordion-Header'>
-                  <div className='from'>
-                    <span>
-                      <AiFillEnvironment size={20}></AiFillEnvironment>
-                    </span>
-                    <p>Mafra, SC</p>
-                  </div>
-                  <span className='arrowIcon'>
-                    <AiOutlineArrowRight size={20}></AiOutlineArrowRight>
-                  </span>
-                  <div className='to'>
-                    <span>
-                      <AiOutlineEnvironment size={20}></AiOutlineEnvironment>
-                    </span>
-                    <p>Itu, SP</p>
-                  </div>
-                </Accordion.Header>
-                <Accordion.Body className="Accordion-Body">
-                  <Card>
-                    <Card.Body class="AccordionCardBody">
-                      <Card.Title class="AccordionCardTitle">
-                        <div className='distance'>
-                          <AiOutlineCar size={19}></AiOutlineCar>
-                          <p>Distância: 526,9 KM</p>
-                        </div>
-                        
-                        <div className="duration">
-                          <AiOutlineHourglass size={19}></AiOutlineHourglass>
-                          <p>Tempo de viagem: 7H 50 min</p>
-                        </div>
-                      </Card.Title>
-                      <Card.Text class="AccordionCardText">
-                        <button className='deleteButton'>
-                          <AiFillDelete size={19}></AiFillDelete>
-                        </button>
-                        <button variant="outline-dark" className='updateButton'>
-                          <AiFillEdit size={19}></AiFillEdit>
-                        </button>
-                      </Card.Text>
-                    </Card.Body>
-                  </Card>
-                </Accordion.Body>
-              </Accordion.Item>
-            </Accordion>
-          </div>
-        </div> 
-
+        <Travels travels={travels}></Travels>
         <Modal></Modal>
         <button onClick={handleShow} class ="createTravelBtn">Nova Viagem</button>
       </header>
